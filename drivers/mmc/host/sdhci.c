82fd93ca37371da4bc5fa9f8b690f37559e94e88
karthick111 <karthickrko61@gmail.com>
2020-04-13 13:53:12 +0530

    drivers: mmc: Import Realme changes
    Change-Id: I813793db7693f67f9f61ba1e0e394e60a2a572fd

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -34,6 +34,10 @@
 
 #include <trace/events/mmc.h>
 
+#ifdef VENDOR_EDIT //Cong.Dai@BSP.TP.Function, 2019/07/03, modified for replace daily build macro
+#include <soc/oppo/oppo_project.h>
+#endif /* VENDOR_EDIT */
+
 #include "sdhci.h"
 #include "cmdq_hci.h"
 
@@ -80,6 +84,12 @@ static void sdhci_dump_state(struct sdhci_host *host)
 
 static void sdhci_dumpregs(struct sdhci_host *host)
 {
+#ifdef VENDOR_EDIT 
+//Cong.Dai@BSP.TP.Function, 2019/07/03, modified for replace daily build macro
+	if(!oppo_daily_build())
+		return;
+#endif
+
 	MMC_TRACE(host->mmc,
 		"%s: 0x04=0x%08x 0x06=0x%08x 0x0E=0x%08x 0x30=0x%08x 0x34=0x%08x 0x38=0x%08x\n",
 		__func__,
@@ -1228,6 +1238,19 @@ void sdhci_send_command(struct sdhci_host *host, struct mmc_command *cmd)
 	if ((host->quirks2 & SDHCI_QUIRK2_STOP_WITH_TC) &&
 	    cmd->opcode == MMC_STOP_TRANSMISSION)
 		cmd->flags |= MMC_RSP_BUSY;
+
+#ifdef VENDOR_EDIT
+//yh@bsp, 2015-10-21 Add for special card compatible
+//Guohua.Zhong@BSP.Storage.Sdcard,20180630 modify for use is_fsck_process whitelist "fsck"
+	if(host->mmc->card_stuck_in_programing_status && mmc_card_is_removable(host->mmc))
+	{
+		printk_once(KERN_INFO "%s:card_stuck_in_programing_status cmd:%d\n", mmc_hostname(host->mmc), cmd->opcode );
+
+		cmd->error = -EIO;
+		sdhci_finish_mrq(host, cmd->mrq);
+		return;
+	}
+#endif /* VENDOR_EDIT */
 
 	/* Wait max 10 ms */
 	timeout = 10000;
